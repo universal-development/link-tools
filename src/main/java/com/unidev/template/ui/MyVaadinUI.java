b78064f2abaaab8fae2b1da0e05e91bce634ddf2
Denis O <denis@universal-development.com>
2015-06-18 15:23:51 +0300

    more test components

--- src/main/java/com/unidev/template/ui/MyVaadinUI.java
+++ src/main/java/com/unidev/template/ui/MyVaadinUI.java
@@ -4,8 +4,10 @@ package com.unidev.template.ui;
 import com.unidev.platform.utils.RandomUtils;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
@@ -16,6 +18,14 @@ public class MyVaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! I'm the root UI! " + randomUtils.randomValue("A", "B", "C")));
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setMargin(true);
+        Button button = new Button("Test button");
+        Label label = new Label("Hello! I'm the root UI! " + randomUtils.randomValue("A", "B", "C"));
+
+        verticalLayout.addComponent(button);
+        verticalLayout.addComponent(label);
+
+        setContent(verticalLayout);
     }
 }
