a2fd3a396c850aa14fc6a9dae2e84a22b68bb103
Denis O <denis@universal-development.com>
2015-06-18 12:43:20 +0300

    test autowired stuff

--- src/main/java/com/unidev/template/ui/MyVaadinUI.java
+++ src/main/java/com/unidev/template/ui/MyVaadinUI.java
@@ -1,15 +1,21 @@
 package com.unidev.template.ui;
 
 
+import com.unidev.platform.utils.RandomUtils;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 public class MyVaadinUI extends UI {
+
+    @Autowired
+    private RandomUtils randomUtils;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! I'm the root UI!"));
+        setContent(new Label("Hello! I'm the root UI! " + randomUtils.randomValue("A", "B", "C")));
     }
 }
