7974ad76267c1706b6908c5fce5682525a3f1a7b
Denis O <denis@universal-development.com>
2015-06-18 12:03:05 +0300

    Porting vaading test app

---
+++ src/main/java/com/unidev/template/TemplateVaadinAppApplication.java
@@ -0,0 +1,12 @@
+package com.unidev.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateVaadinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateVaadinAppApplication.class, args);
+    }
+}
