2eab7b16e64b8f4b142da576f43d4f86cc345576
Denis O <denis@universal-development.com>
2015-06-25 16:42:52 +0300

    Added servlet mapping for /jmx/ and /logs

--- app/src/main/java/com/unidev/template/Application.java
+++ app/src/main/java/com/unidev/template/Application.java
@@ -1,20 +1,36 @@
 package com.unidev.template;
 
+import ch.qos.logback.classic.ViewStatusMessagesServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@EnableWebMvc
 @SpringBootApplication
 @ImportResource(value = {"classpath*:/*-beans.xml"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class we actually don't
+        // need to override this method.
+        return application;
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(new Class[] { Application.class, WebAppInitializer.class }, args);
     }
 }
