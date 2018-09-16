51380e673b00b5e022a8e2c9d044b68878b04ebc
Denis O <denis.o@linux.com>
2018-09-16 20:48:05 +0300

    Ingest service setup

---
+++ linktools-ingest/src/main/java/com.unidev.linktools.ingest/IngestApplication.java
@@ -0,0 +1,12 @@
+package com.unidev.linktools.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestApplication.class, args);
+	}
+}
