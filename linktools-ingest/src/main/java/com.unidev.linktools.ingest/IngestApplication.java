c72690ea62e1fa46d752371f136110d2e4599343
Denis O <denis.o@linux.com>
2018-09-23 09:24:57 +0300

    cli listing buckets

--- linktools-ingest/src/main/java/com.unidev.linktools.ingest/IngestApplication.java
+++ linktools-ingest/src/main/java/com.unidev.linktools.ingest/IngestApplication.java
@@ -1,14 +1,40 @@
 package com.unidev.linktools.ingest;
 
+import com.unidev.linktools.dao.LinkBucketRepository;
+import com.unidev.linktools.model.LinkBucket;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
-public class IngestApplication {
+@EnableMongoRepositories(basePackages="com.unidev.linktools.dao")
+@ComponentScan("com.unidev")
+@Log4j2
+public class IngestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IngestApplication.class, args);
-	}
+    @Autowired
+    public IngestApplication(LinkBucketRepository repository) {
+        this.repository = repository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestApplication.class, args);
+    }
+
+    private final LinkBucketRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Available buckets:");
+
+        for(LinkBucket linkBucket : repository.findAll()) {
+            log.info("{}", linkBucket);
+        }
+    }
 }
