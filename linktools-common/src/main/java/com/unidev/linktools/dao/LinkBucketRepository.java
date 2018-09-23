5afa5d890216bff898bb2181dde6e3c6464d3861
Denis O <denis.o@linux.com>
2018-09-23 08:54:36 +0300

    Repository updates

--- linktools-common/src/main/java/com/unidev/linktools/dao/LinkBucketRepository.java
+++ linktools-common/src/main/java/com/unidev/linktools/dao/LinkBucketRepository.java
@@ -3,6 +3,6 @@ package com.unidev.linktools.dao;
 import com.unidev.linktools.model.LinkBucket;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface LinkBucketDao extends MongoRepository<LinkBucket, String> {
+public interface LinkBucketRepository extends MongoRepository<LinkBucket, String> {
 
 }
--- linktools-common/src/main/java/com/unidev/linktools/model/LinkBucket.java
+++ linktools-common/src/main/java/com/unidev/linktools/model/LinkBucket.java
@@ -3,14 +3,18 @@ package com.unidev.linktools.model;
 import com.unidev.polydata.domain.BasicPoly;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Model class to store info about link buckets
  */
 @Data
 @Builder
+@Document
 public class LinkBucket {
 
+    @Id
     private String name;
 
     private String collection;
